511047da2fa4f6d7986644773af3ff7b5c3ec4c2
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-08-12 08:30:14 -0700

    pushing to github

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const request = require('superagent')
-const weatherData = require('./data/weather.js');
+
 
 const app = express();
 
@@ -35,14 +35,9 @@ app.get('/location', async(req, res) => {
 })
 
 async function getWeather(lat, lon){
-    const requestString = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`
-    console.log(requestString)
-    const response = await request.get(requestString);
-
-
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+    
     const forecast = response.body.data.map((day) => {
-
-
          return{
             forecast: day.weather.description,
             time: new Date(day.ts * 1000),
@@ -50,22 +45,52 @@ async function getWeather(lat, lon){
     })
 
     return forecast;
-    
-    
+        
 };
 
 app.get('/weather', async (req, res) => {
     try {
         const userLat = req.query.latitude;
         const userLon = req.query.longitude;
-        
+
         const mungedData = await getWeather(userLat, userLon);
         res.json(mungedData);
     } catch (e) {
         res.status(500).json({error: e.message})
     }
 });
+// async function getHiking(lat, lon){
+//     const response = await request.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${process.env.TRAIL_API_KEY}`);
+    
+//     const hikes = response.body.trails.map((hike) => {
+//          return{
+//             name: hike.name ,
+//             location:hike.location,
+//             length: hike.length,
+//             stars: hike.stars,
+//             star_votes: hike.starVotes,
+//             summary: hike.summary,
+//             trail_url: hike.url,
+//             conditions: hike.conditionDetails,
+//             condition_date: hike.conditionDate,
+//             condition_time:
+//         };
+//     })
 
+//     return hikes;
+        
+// };
+// app.get('/trails', async (req, res) => {
+//     try {
+//         const userLat = req.query.latitude;
+//         const userLon = req.query.longitude;
+
+//         const mungedData = await getHiking(userLat, userLon);
+//         res.json(mungedData);
+//     } catch (e) {
+//         res.status(500).json({error: e.message})
+//     }
+// });
 
 module.exports = {
     app
